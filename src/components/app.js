f6ad9366607771e2cdadafdd90b6980bae42cb61
Ballred95 <blake.allred95@icloud.com>
2020-08-24 17:45:55 -0600

    added navbar, jumbotron and links

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './home';
+import About from './about';
+import Schedule from './schedule';
+import Navvy from './customNavBar';
 
 export default class App extends Component {
   render() {
     return (
+      <Router>
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <Navvy />
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/about' component={About}/>
+        <Route exact path='/schedule' component={Schedule}/>
       </div>
+      </Router>
     );
   }
 }
--- src/components/home.js
+++ src/components/home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import {Jumbotron, Container, Row, Col, Image, Button } from 'react-bootstrap'
 
 
-export default function (props) {
+export default function Home (props) {
     return (
     <div className=''>
         <Container>
@@ -11,6 +11,11 @@ export default function (props) {
                 <h2>Welcome to code lyfe.io</h2>
                 <p>This is how to build a website with react, react-router and react-bootstrap</p>
             </Jumbotron>
+            <Link to='/about'>
+                <Button variant='danger'>
+                    About
+                </Button>
+            </Link>
         </Container>
     </div>
     )
